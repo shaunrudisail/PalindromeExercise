21691620241e628a40f9db09f235ee33333ed1ce
Shaun Rudisail <sprudisail@gmail.com>
2025-08-04 17:02:25 -0400

    Completed Palindrome Exercise.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string word)
+    {
+        var reversed = "";
+        
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reversed += word[i];
+        }
+
+        if (reversed == word)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("rotator", true)]
+        [InlineData("kayak", true)]
+        [InlineData("apple", false)]
+        [InlineData("jump", false)]
+        [InlineData("generator", false)]
+        [InlineData("traffic", false)]
 
+        public void TestPalindrome(string word, bool expected)
+        {
+            //Arrange
+            var palindrome = new WordSmith();
+            
+            //Act
+            var actual = palindrome.IsAPalindrome(word);
+            
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
